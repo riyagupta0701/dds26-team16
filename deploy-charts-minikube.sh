73829ae1d3d35a11e21013e785c9aa667559073f
Oei, J.T. (Jimmy) <jimmy.oei10@gmail.com>
2026-02-28 14:47:57 +0100

    k8s files and infra

--- deploy-charts-minikube.sh
+++ deploy-charts-minikube.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+set -e   # exit immediately if any command fails
 
+# 1. Install Redis and nginx ingress controller with Helm
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
+helm upgrade --install redis bitnami/redis -f helm-config/redis-helm-values.yaml
 
-helm install -f helm-config/redis-helm-values.yaml redis bitnami/redis
+# 2. Create configmap for internal nginx gateway
+kubectl create configmap gateway-nginx-conf \
+  --from-file=nginx.conf=k8s/gateway-nginx.conf \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+# 3. Deploy the gateway and app services
+kubectl apply -f k8s/
